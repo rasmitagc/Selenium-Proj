2a4a6a2ad1470aca11bbb47de22843da2d4521be
rasmitagc <81323933+rasmitagc@users.noreply.github.com>
2024-06-18 21:56:08 +0545

    Update Settingclass.java

--- Settingclass.java
+++ Settingclass.java
@@ -1,25 +1,18 @@
 package projectauto1;
-
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class Settingclass {
 	
 	public static WebDriver driver;
-	
-	
 	public static ChromeDriver setup() {
 		
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver= new ChromeDriver();
 		return driver;
-
 		
 	}
 	
@@ -29,7 +22,6 @@ public class Settingclass {
 		driver=setup();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.manage().window().maximize();
-		
 		
 	}
 	
